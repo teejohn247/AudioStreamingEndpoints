818b634f86b5116fed89ea2ad254bf97d8eafea6
teejohn247 <teejohn247@gmail.com>
2020-11-27 09:59:03 +0100

    added endpoints to view and delete admin

---
+++ controllers/delAdmin.js
@@ -0,0 +1,31 @@
+import Admin from '../model/OrdinaryAdmin';
+import SuperAdmin from '../model/Admin';
+
+
+
+const delUser = async(req, res) => {
+    try{
+       const result = await SuperAdmin.findOne({_id: req.payload.id});
+       await Admin.deleteOne({ _id: req.params.id});
+       console.log(result)
+       if(!result){
+        {
+            res.status(404).json({
+                status:404,
+                error:'ACCESS DENIED. Only a super-admin is allowed to delete this account'
+            })
+            return
+        }
+       }
+        res.status(200).json({
+            status:200,
+            msg:'Admin account deleted'
+        })
+    }catch(err){
+        res.status(500).json({
+            status:500,
+            err:'server error'
+        })
+    }
+}
+export default delUser;
---
+++ controllers/viewSingleAdmin.js
@@ -0,0 +1,30 @@
+import Admin from '../model/OrdinaryAdmin';
+
+
+const viewSingleAdmin = async(req, res) => {
+    try{
+       const result = await Admin.findOne({_id: req.payload.id});
+       console.log(result)
+       if(!result){
+        {
+            res.status(404).json({
+                status:404,
+                error:'Admin does not exist'
+            })
+            return
+        }
+       }
+        
+        res.status(200).json({
+            status:200,
+            msg:'Success',
+            result
+        })
+    }catch(err){
+        res.status(500).json({
+            status:500,
+            err:'server error'
+        })
+    }
+}
+export default viewSingleAdmin;
